c2b3e734e748069142c09b2462fb25b3f266bde9
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-27 12:23:18 +0300

    feat(layout): added layout components, resolved imports path

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
-import Card from '../UI/Card';
+import Card from '../UI/Card/Card';
 import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import CartItem from './CartItem/CartItem';
 
 const Cart = (props) => {
     return (
