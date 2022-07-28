1bb3db828106b26fa092691114158d163b73a3f3
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-28 16:05:54 +0300

    feat(cart): added effect for making http request when a cart item is saved

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Layout from './components/Layout/Layout';
 import Cart from './components/Cart/Cart';
 import Products from './components/Shop/Products';
 import {useSelector} from 'react-redux';
+import {useEffect} from 'react';
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+      fetch('https://redux-cart-42c3e-default-rtdb.firebaseio.com/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+      })
+  }, [cart])
 
   return (
       <Layout>
